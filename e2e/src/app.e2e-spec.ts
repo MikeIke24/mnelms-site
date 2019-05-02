7f465f1286fc1c249f071e67292366197716b351
MikeIke24 <mnelms48@gmail.com>
2019-05-02 14:22:45 -0700

    First commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ngm-starter!');
+    expect(page.getTitleText()).toEqual('Welcome to mnelms-site!');
   });
 
   afterEach(async () => {
--- src/app/core/app.component.spec.ts
+++ src/app/core/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngm-starter'`, () => {
+  it(`should have as title 'mnelms-site'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngm-starter');
+    expect(app.title).toEqual('mnelms-site');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngm-starter!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mnelms-site!');
   });
 });
